6678ee73f2da4aa66d8dcdecfadf12050b5ff312
Sean <trott.sean@gmail.com>
2025-11-04 11:11:04 -0800

    data and code

---
+++ src/models/utils.py
@@ -0,0 +1,34 @@
+def generate_revisions():
+    ## TODO: Ensure this is correct
+    # Fixed initial steps
+    revisions = [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1000]
+    # Add every 1,000 steps afterward
+    revisions.extend(range(2000, 144000, 1000))  # Adjust range as needed
+    # Format each step as "stepX"
+    return [f"step{step}" for step in revisions]
+
+
+def generate_revisions_test():
+    revisions = [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 
+                 1000, 2000, 4000, 8000, 16000, 32000, 64000, 
+                 100000, 143000]
+    revisions = [143000]
+    return [f"step{step}" for step in revisions]
+
+
+def count_parameters(model):
+    """credit: https://stackoverflow.com/questions/49201236/check-the-total-number-of-parameters-in-a-pytorch-model"""
+    
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        
+        # if the param is not trainable, skip it
+        if not parameter.requires_grad:
+            continue
+        
+        # otherwise, count it towards your number of params
+        params = parameter.numel()
+        total_params += params
+    # print(f"Total Trainable Params: {total_params}")
+    
+    return total_params
